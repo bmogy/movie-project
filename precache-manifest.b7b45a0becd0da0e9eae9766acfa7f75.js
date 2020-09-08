70a094c4a6f317fc12123e1d973c3a34a5a481bb
bmogy <bmogy@outlook.com>
2020-09-08 10:33:04 -0500

    Updates

---
+++ precache-manifest.b7b45a0becd0da0e9eae9766acfa7f75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9991586585f8fcd30ae8bd07e14e713a",
+    "url": "/movie-project/index.html"
+  },
+  {
+    "revision": "a8fe14082d9ba5b77bb2",
+    "url": "/movie-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ce493d52060ffd335f0",
+    "url": "/movie-project/static/js/2.a5e64b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-project/static/js/2.a5e64b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fe14082d9ba5b77bb2",
+    "url": "/movie-project/static/js/main.76a4b76d.chunk.js"
+  },
+  {
+    "revision": "435983f6847c4564dcc2",
+    "url": "/movie-project/static/js/runtime-main.f33c7b3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
